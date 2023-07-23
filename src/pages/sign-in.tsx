0a303e1125a24717902a78b7227a07ce683335a8
unknown <marsanic.david@gmail.com>
2023-07-24 01:59:46 +0200

    fixed error handling and redirect for sign up, still seeing the 500 error code

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,8 +1,7 @@
-import { data } from '@/data/info';
-import UnauthenticatedLayout from '@/layouts/UnauthenticatedLayout';
-import { useSignInEmailPassword, useAuthenticationStatus } from '@nhost/react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useSignInEmailPassword, useAuthenticationStatus } from '@nhost/react';
+import UnauthenticatedLayout from '@/layouts/UnauthenticatedLayout';
 import { useRouter } from 'next/router';
 
 type SignInFormValues = {
@@ -11,16 +10,9 @@ type SignInFormValues = {
 };
 
 export function SignInPage() {
-  const { signInEmailPassword, isLoading } = useSignInEmailPassword();
+  const { signInEmailPassword, isLoading, isError, error } = useSignInEmailPassword();
   const { isAuthenticated } = useAuthenticationStatus();
   const router = useRouter();
-  const [signInError, setSignInError] = useState(null);
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      router.push('/');
-    }
-  }, [isAuthenticated, router]);
 
   const { register, handleSubmit } = useForm<SignInFormValues>({
     reValidateMode: 'onSubmit',
@@ -35,86 +27,90 @@ export function SignInPage() {
 
     try {
       await signInEmailPassword(email, password);
+      if (isAuthenticated) {
+        router.push('/');
+      }
     } catch (err) {
       console.error(err);
-      setSignInError(err.message);
     }
   }
 
   return (
     <UnauthenticatedLayout
       mainContainerProps={{ className: 'py-20' }}
-      title={data.pageTitle}
+      title="Sign In"
     >
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="grid w-full max-w-xs grid-flow-row gap-6 py-8 mx-auto rounded-md"
-      >
-        <div className="grid grid-flow-row gap-2">
-          <label
-            htmlFor="email"
-            className="text-list text-sm font-medium leading-none"
-          >
-            Email
-          </label>
+      <div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="grid w-full max-w-xs grid-flow-row gap-6 py-8 mx-auto rounded-md"
+        >
+          <div className="grid grid-flow-row gap-2">
+            <label
+              htmlFor="email"
+              className="text-list text-sm font-medium leading-none"
+            >
+              Email
+            </label>
 
-          <input
-            {...register('email')}
-            className="bg-input text-list w-full px-2 py-3 text-sm rounded-md"
-            id="email"
-            placeholder="Email"
-            required
-            minLength={2}
-            maxLength={128}
-            spellCheck="false"
-            autoCapitalize="none"
-            type="email"
-          />
-        </div>
+            <input
+              {...register('email')}
+              className="bg-input text-list w-full px-2 py-3 text-sm rounded-md"
+              id="email"
+              placeholder="Email"
+              required
+              minLength={2}
+              maxLength={128}
+              spellCheck="false"
+              autoCapitalize="none"
+              type="email"
+            />
+          </div>
 
-        <div className="grid grid-flow-row gap-2">
-          <label
-            htmlFor="password"
-            className="text-list text-sm font-medium leading-none"
-          >
-            Password
-          </label>
+          <div className="grid grid-flow-row gap-2">
+            <label
+              htmlFor="password"
+              className="text-list text-sm font-medium leading-none"
+            >
+              Password
+            </label>
 
-          <input
-            {...register('password')}
-            className="bg-input text-list w-full px-2 py-3 text-sm rounded-md"
-            id="password"
-            placeholder="Password"
-            required
-            minLength={2}
-            maxLength={128}
-            spellCheck="false"
-            autoCapitalize="none"
-            autoComplete="false"
-            type="Password"
-          />
-        </div>
+            <input
+              {...register('password')}
+              className="bg-input text-list w-full px-2 py-3 text-sm rounded-md"
+              id="password"
+              placeholder="Password"
+              required
+              minLength={2}
+              maxLength={128}
+              spellCheck="false"
+              autoCapitalize="none"
+              autoComplete="false"
+              type="Password"
+            />
+          </div>
 
-        <div className="flex flex-col">
-          <button
-            className="text-list hover:border-white hover:text-white border-list flex items-center justify-center w-full py-2 mt-4 text-sm transition-colors duration-200 border rounded-md"
-            type="submit"
-            disabled={isLoading}
-          >
-            {isLoading ? 'Loading...' : 'Sign In'}
-          </button>
-        </div>
-      </form>
+          <div className="flex flex-col">
+            <button
+              className="text-list hover:border-white hover:text-white border-list flex items-center justify-center w-full py-2 mt-4 text-sm transition-colors duration-200 border rounded-md"
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading ? 'Loading...' : 'Sign In'}
+            </button>
+          </div>
+        </form>
 
-      {signInError && (
-        <div className="w-full max-w-xs mx-auto">
-          <p className="text-red font-medium text-red-500">
-            Error: {signInError}
-          </p>
-        </div>
-      )}
+        {isError && (
+          <div className="w-full max-w-xs mx-auto">
+            <p className="text-red font-medium text-red-500">
+              Error: {error.message}
+            </p>
+          </div>
+        )}
+      </div>
     </UnauthenticatedLayout>
   );
-};
+}
 
 export default SignInPage;
