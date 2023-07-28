73c6988c4aa061447b50f6a47c65c0085209167a
unknown <marsanic.david@gmail.com>
2023-07-28 20:52:54 +0200

    removed broken import from the old sample code

--- src/pages/developers.tsx
+++ src/pages/developers.tsx
@@ -1,5 +1,3 @@
-import { AddNewSpeaker } from '@/components/speakers/AddNewSpeaker';
-import { SpeakersGrid } from '@/components/speakers/SpeakersGrid';
 import { data } from '@/data/info';
 import BaseLayout from '@/layouts/BaseLayout';
 import { useAuthenticationStatus } from '@nhost/react';
@@ -10,14 +8,13 @@ function SpeakersPage() {
 
   return (
     <div className="grid grid-flow-row gap-2 py-4">
-      <SpeakersGrid />
+
 
       {isAuthenticated ? (
         <div className="w-full max-w-lg py-10 mx-auto">
           <h1 className="text-dim pb-8 text-3xl font-medium leading-none text-center">
             Add New Speaker
           </h1>
-          <AddNewSpeaker />
         </div>
       ) : null}
     </div>
